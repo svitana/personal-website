90798964b113cb06bd80541f54922d54dc24de26
Svitana <svetulkin96@gmail.com>
2017-11-17 02:36:45 +0000

    Initial

---
+++ js/form.js
@@ -0,0 +1,23 @@
+$('form input[type="submit"]').click(sendForm);
+
+function sendForm(e) {
+    e.preventDefault();
+    $.ajax({
+        url: "https://formspree.io/svlevytska@gmail.com",
+        method: "POST",
+        data: {
+            name: $('#name').val(),
+            name: $('#mail').val(),
+            name: $('#message').val()
+        },
+        dataType: "json"
+    })
+    .done(function(){
+        $('form')[0].reset();
+        $('#msg').html('Thank you! Your message has been sent.');
+    })
+    .fail(function(){
+        $('#msg').html('Sorry. There was an error sending the message. Please try again.');
+    });
+    }
+
